a530adb1e1787157df9805a65eeae2e608b0f226
刘泰岐 <liutaiqi@meituan.com>
2022-05-05 14:59:16 -0400

    skull

---
+++ HD-BET/HD_BET/paths.py
@@ -0,0 +1,4 @@
+import os
+
+# please refer to the readme on where to get the parameters. Save them in this folder:
+folder_with_parameter_files = os.path.join(os.path.expanduser('/cis/home/tliu77/Documents/training/HD-BET/HD_BET'), 'hd-bet_params')
